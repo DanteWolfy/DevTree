104863044253a26092dd9824a0253ed6f177387c
Carlos Victor <Carllos.victor@hotmail.com>
2024-01-24 20:43:52 -0300

    comments

--- script.js
+++ script.js
@@ -8,9 +8,6 @@ function toggleMode() {
     }
   }
 }
-/*       document.body.classList.toggle('light');      */
-
-/* A presente função me causou problemas não consegui inserir no html, portanto tive que usar uma alternativa */
 
 const img = querySelector("#profile img")
 if (document.body.classList.contains("light")) {
@@ -19,18 +16,3 @@ if (document.body.classList.contains("light")) {
   img.setAttribute("src", "./assets/images/avatar.png")
 }
 
-// animação
-
-var div = document.getElementById("log")
-var texto = "Olá meu nome é Carlos Victor, seja bem vindo!"
-
-function escrever(str, el) {
-  var char = str.split("").reverse()
-  var typer = setInterval(function () {
-    if (!char.length) return clearInterval(typer)
-    var next = char.pop()
-    el.innerHTML += next
-  }, 100)
-}
-
-escrever(texto, div);
